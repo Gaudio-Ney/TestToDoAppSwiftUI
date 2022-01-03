40518c33b24c9ae35e8e9da85751476b240a7d9b
Gáudio Uchôa Ney <gaudio.ney@gmail.com>
2022-01-03 14:26:40 -0300

    Versão de testes do app de ToDo inicializada

---
+++ AppToDo/Preview Content/MinhaClasse.swift
@@ -0,0 +1,14 @@
+//
+//  MinhaClasse.swift
+//  AppToDo
+//
+//  Created by Gáudio Ney on 11/09/20.
+//  Copyright © 2020 Gáudio Ney. All rights reserved.
+//
+
+import Foundation
+
+struct MinhaClasse {
+    var nome:String
+    
+}
